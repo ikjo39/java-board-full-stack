ee3927c667861004288bd3c67d1efb176ace64db
ikjo39 <chi4321@naver.com>
2023-10-25 15:33:44 +0900

    feat: Board 엔티티 추가

---
+++ src/main/java/com/ikjo/fullstackboard/domain/Board.java
@@ -0,0 +1,30 @@
+package com.ikjo.fullstackboard.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Builder
+    private Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
