04c6096580a919ebfa082ac22c42df4ddc564d4f
ikjo39 <chi4321@naver.com>
2023-10-25 15:05:27 +0900

    build: project setting

---
+++ src/main/java/com/ikjo/fullstackboard/FullStackBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ikjo.fullstackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ikjo/fullstackboard/FullStackBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikjo.fullstackboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
